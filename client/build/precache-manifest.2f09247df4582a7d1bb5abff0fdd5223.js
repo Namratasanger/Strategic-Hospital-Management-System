023c859daa78d13f31cb140f6d0e564c3ebffd34
NamrataSanger <namratasanger55@gmail.com>
2020-10-19 09:33:56 +0530

    updates

---
+++ client/build/precache-manifest.2f09247df4582a7d1bb5abff0fdd5223.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "79631210c9ff9bd2294a",
+    "url": "/static/css/main.e9d8192d.chunk.css"
+  },
+  {
+    "revision": "79631210c9ff9bd2294a",
+    "url": "/static/js/main.79631210.chunk.js"
+  },
+  {
+    "revision": "2f5f2496186bba217bfb",
+    "url": "/static/css/1.42f20483.chunk.css"
+  },
+  {
+    "revision": "2f5f2496186bba217bfb",
+    "url": "/static/js/1.2f5f2496.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "54952121a8a7f94c074099e7c1f60f6c",
+    "url": "/static/media/unlock.54952121.svg"
+  },
+  {
+    "revision": "53a44d687b5cb86c608d818c8a8d26da",
+    "url": "/static/media/home-image.53a44d68.svg"
+  },
+  {
+    "revision": "715af3d7dfea266ce0f7d9d914278e6a",
+    "url": "/index.html"
+  }
+];
